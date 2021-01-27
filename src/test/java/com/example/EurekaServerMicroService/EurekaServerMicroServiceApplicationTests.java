9a8d0811fb3997489d289bf14dcd05845333c337
LOKESH159 <lokesh.rao@anywhere.co>
2021-01-27 11:42:07 +0530

    firstcommit

---
+++ src/test/java/com/example/EurekaServerMicroService/EurekaServerMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EurekaServerMicroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
